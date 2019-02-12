ca8e301b02998a84ecd6270147ab223b69bc59e3
mandamurillo <manda-murillo@csumb.edu>
2019-02-12 13:42:47 -0800

    part2

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Maria Evelin Anda-Murillo on 2/12/19.
+//  Copyright © 2019 Maria Evelin Anda-Murillo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
